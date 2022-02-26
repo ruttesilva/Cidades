415dc96b5f0f3db89e7dcc0c04488fd3edec082b
Rutte Silva <rosrosrutte@gmail.com>
2022-02-25 22:36:46 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Peruíbe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("Guarujá");
